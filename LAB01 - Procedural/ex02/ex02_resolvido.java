ebae718a22eb0b4ed5c70d3c503fb545b2ab746c
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-08-21 20:10:10 -0300

    ex02 parcialmente resolvido

--- LAB01 - Procedural/ex02/ex02_resolvido.java
+++ LAB01 - Procedural/ex02/ex02_resolvido.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ex02_resolvido {
+import  java.util.Scanner;
+public class ex02_resolvido {
+    public static void main(String[] args) {
+        String nome[] = new String[4];
+        double preco[] = new double[4];
+        int qtd_estoque[] = new int[4];
+        Scanner leitura = new Scanner(System.in);
+        for(int i = 0; i < 4; i++){
+            System.out.println("Informe o nome, o preco e a quantidade\n");
+            nome[i] = leitura.next();
+            preco[i] = leitura.nextDouble();
+            qtd_estoque[i] = leitura.nextInt();
+        }
+    }
+    public static int altera_preco(float preco, float porcentagem){
+        float temp;
+
+        if(porcentagem > -100){
+            temp = 1 + porcentagem/100;
+            preco = preco * porcentagem;
+            return 0;
+        } else {
+            return  -1;
+        }
+
+
+    }
 }
