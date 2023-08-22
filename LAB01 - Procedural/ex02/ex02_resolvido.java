ac9b05bee2cd8a732eb02358b50e927599e6ab48
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-08-22 10:24:14 -0300

    ex02 finalizado

--- LAB01 - Procedural/ex02/ex02_resolvido.java
+++ LAB01 - Procedural/ex02/ex02_resolvido.java
@@ -11,6 +11,24 @@ public class ex02_resolvido {
             preco[i] = leitura.nextDouble();
             qtd_estoque[i] = leitura.nextInt();
         }
+        System.out.println("Aumentando o preco em 10% do produto 1 e 3");
+        altera_preco((float)preco[0], -110);
+        altera_preco((float)preco[2], 10);
+        System.out.println("Reduzindo o preco em 5% do produto 2");
+        if(altera_preco((float)preco[1], -5) == -1){
+            System.out.println("Erro! Preco nao alterado. Porcentagem invalida");
+        } else {
+            System.out.println("\nPreco alterado com sucesso");
+        }
+
+        System.out.println("\nAlterando o preco do produto 3");
+        if(altera_preco((float)preco[3], -110) == -1){
+            System.out.println("Erro! Preco nao alterado. Porcentagem invalida");
+        }
+        System.out.println("Produtos cadastrados: \n");
+        for(int i = 0; i < 4; i++){
+            System.out.printf("\nProduto: %s\nPreco: %.2f\nEstoque: %d", nome[i], preco[i], qtd_estoque[i]);
+        }
     }
     public static int altera_preco(float preco, float porcentagem){
         float temp;
@@ -20,9 +38,7 @@ public class ex02_resolvido {
             preco = preco * porcentagem;
             return 0;
         } else {
-            return  -1;
+            return -1;
         }
-
-
     }
 }
