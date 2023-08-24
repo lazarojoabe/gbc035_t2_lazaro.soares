b972e5c5055b7f24f78b42de16354ff5a6aa6d44
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-08-24 18:55:27 -0300

    ex02 modified

--- LAB02 - First OOP code/ex02/ex02-resolvido/src/Main.java
+++ LAB02 - First OOP code/ex02/ex02-resolvido/src/Main.java
@@ -3,25 +3,29 @@ public class Main {
         Figurinha a = new Figurinha();
         Figurinha b = new Figurinha();
         Figurinha c = new Figurinha();
-        /* b.nome = "Hulk";
-        b.altura = 1.75f;
-        b.posicao = "Centro-Avante";
-        b.data_nasc = "17/08/1983";
-        b.pais = "Brasil";
-        b.peso = 95.6f;*/
-
-        /*c.nome = "Van Dijk";
-        c.altura = 1.92f;
-        c.posicao = "Zagueiro";
-        c.data_nasc = "07/10/1990";
-        c.pais = "Holanda";
-        c.peso = 83.7f;*/
 
         a.setNome("Lionel Messi");
         a.setAltura(1.67f);
         a.setPais("Argentina");
         a.setPosicao("Meia-armador");
-        a.setPeso(1.67f);
+        a.setPeso(70.5f);
         a.setData_nasc("28/06/1985");
+        a.mostrarIfo();
+
+        b.setNome("Hulk");
+        b.setAltura(1.75f);
+        b.setPais("Brasil");
+        b.setPosicao("Centro-Avante");
+        b.setPeso(95.6f);
+        b.setData_nasc("17/08/1983");
+        b.mostrarIfo();
+
+        c.setNome("Van Dijk");
+        c.setAltura(1.95f);
+        c.setPais("Holanda");
+        c.setPosicao("Zagueiro");
+        c.setPeso(83.7f);
+        c.setData_nasc("07/10/1990");
+        c.mostrarIfo();
     }
 }
