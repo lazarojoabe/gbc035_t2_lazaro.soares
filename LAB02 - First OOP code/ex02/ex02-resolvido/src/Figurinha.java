57ce5775ae81e1c96215756c82f9a98f51737884
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-08-24 12:19:37 -0300

    Terminar ex02

---
+++ LAB02 - First OOP code/ex02/ex02-resolvido/src/Figurinha.java
@@ -0,0 +1,41 @@
+public class Figurinha {
+    private String nome;
+    private String data_nasc;
+    private float peso;
+    private float altura;
+    private String pais;
+    private String posicao;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public void setData_nasc(String data_nasc) {
+        this.data_nasc = data_nasc;
+    }
+
+    public void mostrarIfo(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Nascimento: " + data_nasc);
+        System.out.println("Altura: " + altura);
+        System.out.println("Peso: " + peso);
+        System.out.println("Posição: " + posicao);
+        System.out.println("País: " + pais);
+    }
+}
---
+++ LAB02 - First OOP code/ex02/ex02-resolvido/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Figurinha a = new Figurinha();
+        Figurinha b = new Figurinha();
+        Figurinha c = new Figurinha();
+        /* b.nome = "Hulk";
+        b.altura = 1.75f;
+        b.posicao = "Centro-Avante";
+        b.data_nasc = "17/08/1983";
+        b.pais = "Brasil";
+        b.peso = 95.6f;*/
+
+        /*c.nome = "Van Dijk";
+        c.altura = 1.92f;
+        c.posicao = "Zagueiro";
+        c.data_nasc = "07/10/1990";
+        c.pais = "Holanda";
+        c.peso = 83.7f;*/
+
+        a.setNome("Lionel Messi");
+        a.setAltura(1.67f);
+        a.setPais("Argentina");
+        a.setPosicao("Meia-armador");
+        a.setPeso(1.67f);
+        a.setData_nasc("28/06/1985");
+    }
+}
