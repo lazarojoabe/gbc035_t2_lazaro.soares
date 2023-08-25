d19f9f4997bd6c43eb1644c917e4d660287feba7
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-08-25 14:46:39 -0300

    LAB02 finalizado!

--- LAB02 - First OOP code/ex05/ex05-resolvido/src/Figurinha.java
+++ LAB02 - First OOP code/ex05/ex05-resolvido/src/Figurinha.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Figurinha {
+public class Figurinha {
+        private String nome;
+        private String data_nasc;
+        private float peso;
+        private float altura;
+        private String pais;
+        private String posicao;
+
+        public Figurinha(String pais){
+            this.pais = pais;
+            nome = " ";
+            peso = 0.0f;
+            altura = 0.0f;
+            posicao = " ";
+            data_nasc = " /  /    ";
+        }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setAltura(float altura) {
+            this.altura = altura;
+        }
+
+        public void setPais(String pais) {
+            this.pais = pais;
+        }
+
+        public void setPeso(float peso) {
+            this.peso = peso;
+        }
+
+        public void setPosicao(String posicao) {
+            this.posicao = posicao;
+        }
+
+        public void setData_nasc(String data_nasc) {
+            this.data_nasc = data_nasc;
+        }
+
+        public void mostrarInfo(){
+            System.out.println("Nome: " + nome);
+            System.out.println("Nascimento: " + data_nasc);
+            System.out.println("Altura: " + altura);
+            System.out.println("Peso: " + peso);
+            System.out.println("Posição: " + posicao);
+            System.out.println("País: " + pais);
+        }
 }
--- LAB02 - First OOP code/ex05/ex05-resolvido/src/Selecao.java
+++ LAB02 - First OOP code/ex05/ex05-resolvido/src/Selecao.java
@@ -1,2 +1,44 @@
 public class Selecao {
+    private String nome;
+    private  String tecnico;
+    private int qtd_mundiais;
+    //Figurinha[] jogadores = new Figurinha[11];
+
+    public Selecao(String nome, String tecnico){
+        this.nome = nome;
+        this.tecnico = tecnico;
+        Figurinha[] jogadores = new Figurinha[11];
+        jogadores[0] = new Figurinha(nome);
+        jogadores[1] = new Figurinha(nome);
+        jogadores[2] = new Figurinha(nome);
+        jogadores[3] = new Figurinha(nome);
+        jogadores[4] = new Figurinha(nome);
+        jogadores[5] = new Figurinha(nome);
+        jogadores[6] = new Figurinha(nome);
+        jogadores[7] = new Figurinha(nome);
+        jogadores[8] = new Figurinha(nome);
+        jogadores[9] = new Figurinha(nome);
+        jogadores[10] = new Figurinha(nome);
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTecnico(String tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public void setQtd_mundiais(int qtd_mundiais) {
+        this.qtd_mundiais = qtd_mundiais;
+    }
+
+    public void getInfo(){
+        System.out.println("Seleção da " + nome);
+        System.out.println("Tecnico: " + tecnico);
+        System.out.printf("Ganhou a Copa do Mundo %d vezes\n", qtd_mundiais);
+        System.out.println("Jogadores titulares: ");
+        //for(int i = 0; i < 11; i++){
+        //    jogadores[i].mostrarInfo();
+        //}
+    }
 }
