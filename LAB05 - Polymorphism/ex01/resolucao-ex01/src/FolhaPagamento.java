e2d64f42e8164271a3556032d8c7a29b2bef7c94
Lázaro Joabe <99762355+lazarojoabe@users.noreply.github.com>
2023-10-05 18:30:09 -0300

    ex01 finalizado com sucesso!

--- LAB05 - Polymorphism/ex01/resolucao-ex01/src/FolhaPagamento.java
+++ LAB05 - Polymorphism/ex01/resolucao-ex01/src/FolhaPagamento.java
@@ -1,6 +1,6 @@
 public class FolhaPagamento {
 
-    public static void mostrarFolha(int numF, Funcionario[] vet){
+    public static void mostrarFolha(Funcionario[] vet){
         for(Funcionario f: vet){
             System.out.println("Nome: " + f.getNome());
             System.out.println("Salario: " + f.calcSalario());
